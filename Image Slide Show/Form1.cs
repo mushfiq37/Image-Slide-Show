ec7766b0d6d38bc353f02c74b2f568a985ef35df
unknown <mushfiq96.cse.ku@gmail.com>
2018-04-28 00:34:26 +0600

    ImageSlideShow

---
+++ Image Slide Show/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Slide_Show
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            pictureBox1.Visible = true;
+            for (int i = 1; ;i++)
+            {
+
+                pictureBox1.ImageLocation = @"C:\Users\Mushfiq\Desktop\pic\i.jpg";
+                
+            }
+            stopWatch.Start();
+        }
+
+        private void stopWatch_Tick(object sender, EventArgs e)
+        {
+        /*    if (pictureBox1.Visible == true)
+            {
+                pictureBox1.Visible = false;
+                pictureBox5.Visible = true;
+            }
+            else if (pictureBox5.Visible == true)
+            {
+                pictureBox5.Visible = false;
+                pictureBox7.Visible = true;
+            }
+            else if (pictureBox7.Visible == true)
+            {
+                pictureBox7.Visible = false;
+                pictureBox4.Visible = true;
+            }
+            else if (pictureBox4.Visible == true)
+            {
+                pictureBox4.Visible = false;
+                pictureBox6.Visible = true;
+            }
+            else if (pictureBox6.Visible == true)
+            {
+                pictureBox6.Visible = false;
+                pictureBox3.Visible = true;
+            }
+            else if (pictureBox3.Visible == true)
+            {
+                pictureBox3.Visible = false;
+                pictureBox8.Visible = true;
+            }
+            else if (pictureBox8.Visible == true)
+            {
+                pictureBox8.Visible = false;
+                pictureBox2.Visible = true;
+            }
+            else if (pictureBox2.Visible == true)
+            {
+                pictureBox2.Visible = false;
+                pictureBox1.Visible = true;
+            }*/
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
